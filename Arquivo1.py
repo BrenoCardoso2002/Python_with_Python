2731d8ffadf5e5a842dd6ad9c824c6b12db8ef6a
Breno Cardoso <81375008+BrenoCardoso2002@users.noreply.github.com>
2023-04-04 13:59:53 -0300

    Create Arquivo1.py

---
+++ Arquivo1.py
@@ -0,0 +1,6 @@
+# área das importações:
+import subprocess
+
+# Aqui o outro programa é iniciado
+subprocess.run(['python3', 'caminho_do_arquivo/arquivo2.py']) 
+# primeiro é colocado a versão do python instalada no sistema e depois o caminho do arquivo a ser executado.
